567f5685982c57c443e372ea59724d4f36422c09
tj <thor.jarle.kinstad@felleskjopet.no>
2024-12-03 21:42:55 +0100

    clean up code

--- src/aoc/2024/d02/day_02.py
+++ src/aoc/2024/d02/day_02.py
@@ -1,61 +1,29 @@
-from collections import Counter, defaultdict, deque
-
 import aoc_helper
-from aoc_helper import (
-    Grid,
-    PrioQueue,
-    SparseGrid,
-    decode_text,
-    extract_ints,
-    extract_iranges,
-    extract_ranges,
-    extract_uints,
-    frange,
-    irange,
-    iter,
-    list,
-    map,
-    multirange,
-    range,
-    search,
-    tail_call,
-)
+from aoc_helper import list, map, range
 
 raw = aoc_helper.fetch(2, 2024)
 
 
 def parse_raw(raw: str):
     groups = raw.strip().split("\n")
-    parsed_groups = [list(map(int, group.split())) for group in groups]
-    return parsed_groups
+    return [list(map(int, group.split())) for group in groups]
 
 
 data = parse_raw(raw)
 
 
 def all_desc_or_asc(lst):
-    increasing = True
-    decreasing = True
-    for i in range(1, len(lst)):
-        if lst[i] < lst[i - 1]:
-            increasing = False
-        if lst[i] > lst[i - 1]:
-            decreasing = False
-    return increasing or decreasing
+    return all(lst[i] >= lst[i - 1] for i in range(1, len(lst))) or all(
+        lst[i] <= lst[i - 1] for i in range(1, len(lst))
+    )
 
 
 def pair_is_valid(lst):
-    for i in range(1, len(lst)):
-        diff = abs(lst[i] - lst[i - 1])
-        if diff < 1 or diff > 3:
-            return False
-    return True
+    return all(1 <= abs(lst[i] - lst[i - 1]) <= 3 for i in range(1, len(lst)))
 
 
 def valid_check(lst):
-    if all_desc_or_asc(lst) and pair_is_valid(lst):
-        return True
-    return False
+    return all_desc_or_asc(lst) and pair_is_valid(lst)
 
 
 def problem_dampner(data):
@@ -64,9 +32,8 @@ def problem_dampner(data):
         if valid_check(lst):
             valid_counter += 1
         else:
-            original_lst = lst[:]
-            for i in range(len(original_lst)):
-                new_lst = original_lst[:i] + original_lst[i + 1 :]
+            for i in range(len(lst)):
+                new_lst = lst[:i] + lst[i + 1 :]
                 if valid_check(new_lst):
                     valid_counter += 1
                     break
@@ -74,18 +41,11 @@ def problem_dampner(data):
 
 
 def part_one(data=data):
-    valid = 0
-    for lst in data:
-        if valid_check(lst):
-            valid += 1
-    print(valid)
-    return valid
+    return sum(1 for lst in data if valid_check(lst))
 
 
 def part_two(data=data):
-    valid = problem_dampner(data)
-    print(valid)
-    return valid
+    return problem_dampner(data)
 
 
 aoc_helper.lazy_test(day=2, year=2024, parse=parse_raw, solution=part_one)
